7b1302784338404680a9b1949892085cba30425c
FranciscoRodriguez770 <Francisco.Rodriguez770@comunidadunir.net>
2022-01-17 14:02:32 +0000

    cambio URL

--- pipelines/common-steps/integration.sh
+++ pipelines/common-steps/integration.sh
@@ -2,5 +2,5 @@
 
 source todo-list-aws/bin/activate
 set -x
-export BASE_URL=$1
+export BASE_URLexport BASE_URL=https://c1fitp83pc.execute-api.us-east-1.amazonaws.com/Prod
 pytest -s test/integration/todoApiTest.py
