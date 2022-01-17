39c6bd1f61632068585a880648a8408194d3e660
FranciscoRodriguez770 <Francisco.Rodriguez770@comunidadunir.net>
2022-01-17 14:13:05 +0000

    integration.sh v2

--- pipelines/common-steps/integration.sh
+++ pipelines/common-steps/integration.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source todo-list-aws/bin/activate
 set -x
-export BASE_URLexport BASE_URL=https://c1fitp83pc.execute-api.us-east-1.amazonaws.com/Prod
+export BASE_URL=$1
 pytest -s test/integration/todoApiTest.py
